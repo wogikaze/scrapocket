7223366732fef368962e9d7efddfa45a4adb3bb4
wogikaze <kouse2319@gmail.com>
2025-03-20 11:42:57 +0900

    improve scrapocket

--- command_chain/scrapocket.js
+++ command_chain/scrapocket.js
@@ -1,34 +1,35 @@
-javascript: (() => {
-    const PROJECT = 'wogikaze-study';  // Webページを保存するproject
+const PROJECT = 'wogikaze-study';  // Webページを保存するproject
 
-    const title = document.title.split(" - ")[0];
-    const lines = [];
-    const quote = window.getSelection().toString().trim();  // 選択範囲の文字列を取得
-    let formattedtitle = "";
-    const decodeURI = decodeURIComponent(window.location.href);
-    if (decodeURI.match("scrapbox.io/")) {
-        const re = /scrapbox.io\/(.+)/;
-        formattedtitle = `[/${decodeURI.match(re)[1]}]`;
+const title = document.title.split(" - ")[0];
+const lines = [];
+const quote = window.getSelection().toString().trim();  // 選択範囲の文字列を取得
+const decodeURI = decodeURIComponent(window.location.href);
+const formattedtitle = (() => {
+    const isScrapbox = decodeURI.match("scrapbox.io/");
+    const isYoutube = decodeURI.match("youtube.com/watch");
+
+    switch (true) {
+        case isScrapbox: {
+            const re = /scrapbox.io\/(.+)/;
+            return `[/${decodeURI.match(re)[1]}]`;
+        }
+        case isYoutube: {
+            const re = /watch\?v=(.+)&?/;
+            return `[https://www.youtube.com/watch?v=${decodeURI.match(re)[1]}]`;
+        }
+        default:
+            return `[. ${decodeURI}]${title}`;
     }
-    else if (decodeURI.match("youtube.com/watch")) {
-        const re = /watch\?v=(.+)&?/;
-        formattedtitle = `[https://www.youtube.com/watch?v=${decodeURI.match(re)[1]}]`;
-    }
-    else {
-        formattedtitle = `[. ${decodeURI}]${title}`;
-    }
-    lines.push(
-        formattedtitle
+})();
+
+lines.push(formattedtitle);
+if (quote) {
+    lines.push(...quote
+        .split(/[\f\n\r]/g)  // 改行区切りで配列化
+        .filter(line => line !== '')  // 空行は削除
+        .map(line => `>${line}`),  // 引用記号
+        '', // 空行
     );
-    if (quote) {
-        lines.push(...quote
-            .split(/[\f\n\r]/g)  // 改行区切りで配列化
-            .filter(line => line !== '')  // 空行は削除
-            .map(line => `>${line}`),  // 引用記号
-            '', // 空行
-        );
-    }
-    const body = encodeURIComponent(lines.join('\n'));
-    window.open(`https://scrapbox.io/${PROJECT}/${title}?body=${body}`, '_blank'), history.replaceState && history.replaceState({}, null, location.href);
-
-})()
+}
+const body = encodeURIComponent(lines.join('\n'));
+window.open(`https://scrapbox.io/${PROJECT}/${title}?body=${body}`, '_blank'), history.replaceState && history.replaceState({}, null, location.href);
